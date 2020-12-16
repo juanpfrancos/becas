04cb52f983b4423fe94a3c9b8b7ca0ba2bfa2a9a
juanpfrancos <juanpanime@gmail.com>
2020-12-16 11:31:08 -0500

    Enhance redirection.

---
+++ interact.js
@@ -0,0 +1,31 @@
+function visitarCarolina() {
+    var x = document.getElementById("becas1");
+    x.style.display = "none";
+
+    var x = document.getElementById("carolina1");
+    x.style.display = "block";
+  }
+
+function visitaraecid() {
+    var x = document.getElementById("becas1");
+    x.style.display = "none";
+
+    var x = document.getElementById("eacid1");
+    x.style.display = "block";
+  }
+
+function volverCarolina() {
+    var x = document.getElementById("carolina1");
+    x.style.display = "none";
+
+    var x = document.getElementById("becas1");
+    x.style.display = "block";
+  }
+
+function volverAecid() {
+    var x = document.getElementById("eacid1");
+    x.style.display = "none";
+
+    var x = document.getElementById("becas1");
+    x.style.display = "block";
+  }
